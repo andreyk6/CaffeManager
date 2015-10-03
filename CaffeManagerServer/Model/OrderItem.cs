f13c36ab790e5aa9e533d3cdfd2ce88aa92481fc
andreyk6 <andreyk6@gmail.com>
2015-10-04 00:44:40 +0300

    Fix TotalPrice calculation in OrderItem

--- CaffeManagerServer/Model/OrderItem.cs
+++ CaffeManagerServer/Model/OrderItem.cs
@@ -8,13 +8,25 @@ namespace CaffeManagerServer.Model
     public class OrderItem
     {
         public int Id { get; set; }
-        public decimal Count { get; set; }
+        public decimal Count
+        {
+            get
+            {
+                return Count;
+            }
+            set
+            {
+                TotalPrice = MenuItem.Price * value;
+                Count = value;
+            }
+        }
+
+        public int MenuItemId { get; set; }
         public virtual MenuItem MenuItem { get; set; }
+
+        public int OrderId { get; set; }
         public virtual Order Order { get; set; }
 
-        public decimal TotalPrice ()
-        {
-            return MenuItem.Price * Count;
-        }
+        public decimal TotalPrice { get; private set; }
     }
 }
