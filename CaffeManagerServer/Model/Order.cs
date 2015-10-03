79b8f7eb82809031a8faf9948f4cc57fe8451401
andreyk6 <andreyk6@gmail.com>
2015-10-04 00:43:52 +0300

    Add time stamp to Order

--- CaffeManagerServer/Model/Order.cs
+++ CaffeManagerServer/Model/Order.cs
@@ -8,7 +8,15 @@ namespace CaffeManagerServer.Model
     public class Order
     {
         public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public int CashierId { get; set; }
         public virtual Cashier Cashier { get; set; }
         public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+        public Order()
+        {
+            Date = DateTime.Now;
+        }
     }
 }
