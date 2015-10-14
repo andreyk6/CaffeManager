33666fb42a376201a3a924a9abb9d616ed461bfe
andreyk6 <andreyk6@gmail.com>
2015-10-15 01:27:35 +0300

    Add Token based authorization

---
+++ CaffeManagerServer/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CaffeManagerServer.App_Start
+{
+    public static class WebApiConfig
+    {
+        public const string DEFAULT_ROUTE_NAME = "MyDefaultRoute";
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ CaffeManagerServer/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using System;
+using System.Security.Claims;
+
+namespace CaffeManagerServer
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            
+            //TODO: Search for user in DB
+
+            //using (AuthRepository _repo = new AuthRepository())
+            //{
+            //    IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+            //    if (user == null)
+            //    {
+            //        context.SetError("invalid_grant", "The user name or password is incorrect.");
+            //        return;
+            //    }
+            //}
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+        }
+    }
+}
